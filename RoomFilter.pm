a04f0d89fa1fee21c4c883f7e873f7d023d5b63e
Jeff King <peff@peff.net>
2011-03-28 13:40:43 -0400

    RoomFilter: treat filters as regular expressions
    You can now do "camptail -r danger.room"" instead of "The
    Danger Room" (or even just "-r danger"). Which is way
    handier.

--- RoomFilter.pm
+++ RoomFilter.pm
@@ -3,13 +3,33 @@ use strict;
 
 sub new {
   my $self = bless {}, shift;
-  $self->{index} = { map { $_ => 1 } @_ };
+  foreach (@_) {
+    if (/^[0-9]+$/) {
+      $self->{id}->{$_} = 1;
+    }
+    else {
+      push @{$self->{re}}, qr/$_/i;
+    }
+  }
   return $self;
 }
 
 sub filter {
   my $self = shift;
-  return grep { $self->{index}->{$_->name} || $self->{index}->{$_->id} } @_;
+  return grep { $self->match($_) } @_;
+}
+
+sub match {
+  my ($self, $room) = @_;
+
+  return 1 if $self->{id}->{$room->id};
+
+  my $name = $room->name;
+  foreach my $re (@{$self->{re}}) {
+    return 1 if $name =~ $re;
+  }
+
+  return 0;
 }
 
 1;
